48053b1a41401720e8f1934a7c0161b48ac9e02e
@_Ingoat <igolwb@gmail.com>
2025-05-09 11:29:43 -0300

    integração do back

---
+++ backend/routes/alunoRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+  buscarAlunos,
+  buscarAlunoId,
+  adicionarAluno,
+  atualizarAluno,
+  deletarAluno,
+} from "../controller/alunoController.js";
+
+const router = express.Router();
+
+router.get("/", buscarAlunos);
+
+router.get("/:id", buscarAlunoId);
+
+router.post("/", adicionarAluno);
+
+router.put("/:id", atualizarAluno);
+
+router.delete("/:id", deletarAluno);
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import alunoRoutes from "./routes/alunoRoutes.js";
+import { sql } from "./config/db.js";                          // Importa a configuração do banco de dados do arquivo db.js
+
+dotenv.config();                                    
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());                       
+app.use(cors());
+app.use(helmet()); 
+app.use(morgan("dev"));
+
+app.use("/api/alunos", alunoRoutes); // Rota de teste que responde com "Hello World!" quando acessada.
+
+async function startdb() {
+  try {
+    await sql`
+    CREATE TABLE IF NOT EXISTS alunos (
+      id SERIAL PRIMARY KEY,
+      nome VARCHAR(255) NOT NULL,
+      notamat DECIMAL(2, 1) NOT NULL,
+      notaport DECIMAL(2, 1) NOT NULL,
+      notahist DECIMAL(2, 1) NOT NULL,
+      notamedia DECIMAL(2, 1) NOT NULL
+    )
+    `;
+
+    console.log("db conectada");
+  } catch (error) {
+    console.error("Erro ao conectar ao banco de dados:", error); // Loga o erro caso a conexão falhe
+  }
+}
+
+startdb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`); // Loga a porta em que o servidor está rodando
+  });
+});
