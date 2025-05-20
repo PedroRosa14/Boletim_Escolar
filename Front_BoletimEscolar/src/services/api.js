c2a66b7b0e1a3a3145afcafaa2e09fc4750a2c93
@_Ingoat <igolwb@gmail.com>
2025-05-20 15:55:54 -0300

    implementando funções de getById, put e post na api

--- Front_BoletimEscolar/src/services/api.js
+++ Front_BoletimEscolar/src/services/api.js
@@ -7,6 +7,34 @@ export const useBoletim = create((set , get) => ({
     alunos:[],
     loading:false,
     error:null,
+    atualAluno: null,
+
+    formData: {
+        nome:"",
+        notamat:"",
+        notaport:"",
+        notahist:"",
+        notamedia:"",
+    },
+
+    setFormData: (formData) => set({formData}),
+    resetForm: () => set({ formData: {nome: "", notamat:"", notaport:"", notahist:"", notamedia:""  }}),
+
+    addAluno: async (e) => {
+        e.preventDefault();
+        set({loading:true});
+        try {
+            const {formData} = get();
+            await axios.post(`${URL}/api/alunos`, formData,);
+            await get().fetchAlunos();
+            get.resetForm();
+            console.log("Aluno adicionado com sucesso");
+        } catch (error) {
+            console.error("Erro ao adicionar aluno:", error);
+        }finally{
+            set({loading:false})
+        }
+    },
 
     //get
     fetchAlunos: async () => {
@@ -22,15 +50,45 @@ export const useBoletim = create((set , get) => ({
         }
     },
 
+    //get id
+    fetchAlunoById: async (id) => {
+        set({loading:true});
+        try {
+            const response = await axios.get(`${URL}/api/alunos/${id}`);
+            set({atualAluno: response.data.data, formData: response.data.data, error:null,});
+        } catch (error) {
+            console.error("Erro ao buscar aluno:", error);
+            set({error: "Erro ao buscar aluno", atualAluno: null});
+        }finally{
+            set({loading:false})
+        }
+    },
+
     //delete
     deleteAluno: async (id) => {
         set({loading:true});
         try {
-            await axios.delete(`${URL}/api/alunos/2`);
+            await axios.delete(`${URL}/api/alunos/${id}`);
             set(prev => ({ alunos: prev.alunos.filter(aluno => aluno.id !== id)}));
             console.log("Aluno deletado com sucesso");
         } catch (error) {
             console.error("Erro ao deletar aluno:", error);
+        }finally{
+            set({loading:false})
+        }
+    },
+
+    updateAluno: async (id) => {
+        set({loading:true});
+        try {
+            const {formData} = get();
+            const response = await axios.put(`${URL}/api/alunos/${id}`, formData);
+            set({atualAluno: response.data.data});
+            await get().fetchAlunos();
+            get.resetForm();
+            console.log("Aluno atualizado com sucesso");
+        } catch (error) {
+            console.error("Erro ao atualizar aluno:", error);
         }finally{
             set({loading:false})
         }
