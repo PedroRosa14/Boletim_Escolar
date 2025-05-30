a7ad4b3f157251a13ef08791611b005b5b650f46
Pedro <167037774+PedroRosa14@users.noreply.github.com>
2025-05-30 08:33:13 -0300

    Add files via upload

---
+++ backend/controller/alunoController.js
@@ -0,0 +1,126 @@
+import { sql } from "../config/db.js";
+
+export const buscarAlunos = async (req, res) => {
+  try {
+    const alunos = await sql`
+        SELECT * FROM alunos
+        ORDER BY id DESC
+        `;
+
+    console.log("alunos: ", alunos);
+    res.status(200).json({ success: true, data: alunos });
+  } catch (error) {
+    console.error("Erro ao buscar alunos:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Erro interno no servidor" });
+  }
+};
+
+export const adicionarAluno = async (req, res) => {
+  const { nome, notamat, notaport, notahist, notamedia, url } = req.body;
+
+  if (!nome || !notamat || !notaport || !notahist || !notamedia ) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Preencha todos os campos!" });
+  }
+
+  try {
+    const novoAluno = await sql`
+        INSERT INTO alunos (nome, notamat, notaport, notahist, notamedia, url)
+        VALUES (${nome}, ${notamat}, ${notaport}, ${notahist}, ${notamedia}, ${url})
+        RETURNING *;
+        `;
+    console.log("Novo aluno adicionado:", novoAluno);
+    res.status(201).json({ success: true, data: novoAluno[0] });
+  } catch (error) {
+    console.error("Erro ao adicionar aluno:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Erro interno no servidor" });
+  }
+};
+
+export const buscarAlunoId = async (req, res) => {
+  const { id } = req.params;
+  const idNumerico = parseInt(id, 10);
+
+  if (isNaN(idNumerico)) {
+    return res.status(400).json({
+      success: false,
+      message: "O ID do aluno deve ser um número inteiro válido.",
+    });
+  }
+
+  try {
+    const aluno = await sql`
+      SELECT * FROM alunos WHERE id = ${idNumerico}
+    `;
+
+    if (aluno.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Aluno não encontrado" });
+    }
+
+    res.status(200).json({ success: true, data: aluno[0] });
+  } catch (error) {
+    console.error("Erro ao buscar aluno:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Erro interno no servidor" });
+  }
+};
+export const atualizarAluno = async (req, res) => {
+  const { id } = req.params;
+  const { nome, notamat, notaport, notahist, notamedia, url } = req.body;
+
+  try {
+    const alunoAtualizado = await sql`
+        UPDATE  alunos SET nome = ${nome}, notamat = ${notamat}, notaport = ${notaport}, notahist = ${notahist}, notamedia = ${notamedia}, url = ${url}
+        WHERE id = ${id} 
+        RETURNING *
+        `;
+
+    if (alunoAtualizado.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Aluno não encontrado" });
+    }
+
+    res.status(200).json({ success: true, data: alunoAtualizado[0] });
+  } catch (error) {
+    console.error("Erro ao atualizar aluno:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Erro interno no servidor" });
+  }
+};
+
+export const deletarAluno = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const alunoDeletado = await sql`
+        DELETE FROM alunos WHERE id = ${id}
+        RETURNING *;
+        `;
+
+    if (alunoDeletado.length === 0) {
+      return res
+        .status(404)
+        .json({ success: false, message: "aluno não encontrado" });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: alunoDeletado[0],
+    });
+  } catch (error) {
+    console.error("Erro ao deletar aluno:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Erro interno no servidor" });
+  }
+};
---
+++ backend/server.js
@@ -0,0 +1,64 @@
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import cors from 'cors';
+import path from 'path';
+import YAML from 'yamljs';
+import swaggerUi from 'swagger-ui-express';
+
+import alunoRoutes from "./routes/alunoRoutes.js";
+import { sql } from "./config/db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Carregue a especificação Swagger do arquivo (ajuste o caminho conforme necessário)
+const swaggerDocument = YAML.load(path.join(process.cwd(), 'docs', 'swagger.yaml')); // Assumindo que swagger.yaml está na pasta 'docs' na raiz do projeto
+
+// Middlewares
+app.use(cors({
+  origin: 'https://ls2nml4-anonymous-8081.exp.direct', // Substitua pela URL do seu frontend no Render quando for para produção
+}));
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("dev"));
+
+// Rota para acessar a documentação Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Rotas da sua API principal
+app.use("/", alunoRoutes);
+
+
+
+// Criação da tabela no banco se não existir
+async function startdb() {
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS alunos (
+        id SERIAL PRIMARY KEY,
+        nome VARCHAR(255) NOT NULL,
+        notamat DECIMAL(2, 1) NOT NULL,
+        notaport DECIMAL(2, 1) NOT NULL,
+        notahist DECIMAL(2, 1) NOT NULL,
+        notamedia DECIMAL(2, 1) NOT NULL,
+        url VARCHAR(255)
+      )
+    `;
+
+    console.log("Banco de dados conectado com sucesso.");
+  } catch (error) {
+    console.error("Erro ao conectar ao banco de dados:", error);
+  }
+}
+
+// Inicializa servidor
+startdb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+  });
+});
